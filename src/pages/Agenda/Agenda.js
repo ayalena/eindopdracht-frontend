1ab3da82a0de000a90449f8accbee655ff6e6653
ayalena <82506563+ayalena@users.noreply.github.com>
2021-11-02 13:30:14 +0100

    basic styling en opzet

--- src/pages/Agenda/Agenda.js
+++ src/pages/Agenda/Agenda.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import logo from "../../assets/logo.png";
 import TimeSlot from "../../components/TimeSlot/TimeSlot";
@@ -12,6 +12,51 @@ function Agenda() {
         history.push("/intake-form")
     }
 
+    const [week, setWeek] = useState([]);
+    const [offsetNumber, setOffsetNumber] = useState(0);
+
+    useEffect(() => {
+        async function fetchWeek() {
+            try {
+                //method to get days of the week
+                // console.log(result.data);
+                setWeek();
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        fetchWeek();
+    }, []);
+
+    function handleNextWeek() {
+        setOffsetNumber(offsetNumber + 7);
+    }
+
+    function handleLastWeek() {
+        // console.log("current", offsetNumber);
+        if (offsetNumber >= 7) {
+            setOffsetNumber(offsetNumber - 7);
+        } else {
+            setOffsetNumber(0);
+        }
+    }
+
+    useEffect(() => {
+        async function fetchNextWeek() {
+            try {
+                //method to get data of next week
+                // console.log(result.data.results);
+                setWeek();
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        if (offsetNumber !== null && offsetNumber !== undefined) {
+            fetchNextWeek();
+        }
+    }, [offsetNumber]);
+
     return (
         <>
             <PageHeader icon={logo} title="Agenda"/>
@@ -19,7 +64,11 @@ function Agenda() {
 
 
                 {/*Button should trigger so that the current date is adjusted to that of the last week, as well as the time slots. If the date is set on the current week, this button should be disabled/invisible*/}
-                <button type="button">
+                <button
+                    type="button"
+                    onClick={handleLastWeek}
+                    disabled={offsetNumber === 0}
+                >
                     Last week
                 </button>
 
@@ -69,7 +118,10 @@ function Agenda() {
                 />
 
                 {/*Button should trigger so that the current date is adjusted to that of the next week, as well as the time slots*/}
-                <button type="button">
+                <button
+                    type="button"
+                    onClick={handleNextWeek}
+                >
                     Next week
                 </button>
             </main>
